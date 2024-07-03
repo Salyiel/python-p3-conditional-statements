3b1831631e26ee9dd838b2c4291d4bb863b49b0d
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-07-03 14:14:47 +0300

    Complete lab assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "brisk"
+    elif 40 <= temperature <= 65:
+        return "a little chilly"
+    elif temperature > 85:
+        return "too dang hot"
+    else:
+        return "perfect"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Cannot divide by zero"
+    else:
+        return "Invalid operation"
